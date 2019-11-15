bfee2bd9dc0cc098190c6ce30e6075fe5bb6319b
AutomicDemo <36577894+AutomicDemo@users.noreply.github.com>
2019-11-14 23:36:47 -0800

    Add experimental area to troubleshoot problems.

--- aemain-v1/capture/create-demo-images.bat
+++ aemain-v1/capture/create-demo-images.bat
@@ -1,5 +1,5 @@
-start /b capture\create_image.bat aemain-test1 aemain-cdrive %1
-start /b capture\create_image.bat aemain-d-new-drive aemain-ddrive %1
-start /b capture\create_image.bat aejenkins-v1 aejenkins %1
-start /b capture\create_image.bat aecdd1-v1 aecdd1 %1
-start /b capture\create_image.bat aecdd2-v1 aecdd2 %1
+start capture\create_image.bat aemain-test1 aemain-cdrive %1
+start capture\create_image.bat aemain-d-new-drive aemain-ddrive %1
+start capture\create_image.bat aejenkins-v1 aejenkins %1
+start capture\create_image.bat aecdd1-v1 aecdd1 %1
+start capture\create_image.bat aecdd2-v1 aecdd2 %1
