e0811a8d84a0474b659310a331695b76373baa19
AutomicDemo <36577894+AutomicDemo@users.noreply.github.com>
2019-11-18 10:50:23 -0800

    Prep for RC

--- aemain-v1/templates/td.bat
+++ aemain-v1/templates/td.bat
@@ -1,1 +1,2 @@
 gcloud deployment-manager deployments create aemain-demo-%1 --config aemain-v1-config.yaml --preview
+exit
---
+++ aemain-v1/templates/tu.sh
@@ -0,0 +1,2 @@
+gcloud deployment-manager deployments update aemain-demo-$1 --config aemain-v1-config.yaml --preview
+exit
